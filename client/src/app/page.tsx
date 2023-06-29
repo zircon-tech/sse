d1727f116057933ebe330dbc2a6780a75e125f27
Alejandro Barrios <ottoabarriosp@hotmail.com>
2023-06-29 19:44:45 -0400

    feat(client): send file

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,28 @@
 "use client";
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 
 export default function Home() {
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+  const [file, setFile] = useState<File | undefined>(undefined);
+
+  const loadFile = ({ target }: any) => {
+    const [currentFile] = target.files;
+    setFile(currentFile);
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    if (!file) return;
+
+    const formData = new FormData();
+    formData.append("file", file, file.name);
+
+    try {
+      await fetch("http://localhost:3001/upload", {
+        method: "POST",
+        body: formData,
+      });
+    } catch {}
   };
 
   return (
@@ -12,8 +31,18 @@ export default function Home() {
         onSubmit={handleSubmit}
         className="w-[20%] bg-slate-50 rounded-md border-gray-500 border-solid border p-4 m-auto"
       >
-        <input type="file" placeholder="archivo" className="my-2"/>
-        <button type="submit" className="bg-slate-200 hover:bg-slate-300 rounded-md p-2">Enviar</button>
+        <input
+          type="file"
+          placeholder="archivo"
+          className="my-2"
+          onChange={loadFile}
+        />
+        <button
+          type="submit"
+          className="bg-slate-200 hover:bg-slate-300 rounded-md p-2"
+        >
+          Enviar
+        </button>
       </form>
     </main>
   );
