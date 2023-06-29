c56de08b3cc81148b0f8f08442ca5d29037e09f7
Alejandro Barrios <ottoabarriosp@hotmail.com>
2023-06-29 18:28:28 -0400

    feat(client): define initial form

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,20 @@
-import Image from 'next/image'
+"use client";
+import { FormEvent } from "react";
 
 export default function Home() {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
-    <main>
-      <h1>Home</h1>
+    <main className="w-screen h-screen flex justify-center items-center">
+      <form
+        onSubmit={handleSubmit}
+        className="w-[20%] bg-slate-50 rounded-md border-gray-500 border-solid border p-4 m-auto"
+      >
+        <input type="file" placeholder="archivo" className="my-2"/>
+        <button type="submit" className="bg-slate-200 hover:bg-slate-300 rounded-md p-2">Enviar</button>
+      </form>
     </main>
-  )
+  );
 }
