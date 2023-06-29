ce40d582eae6663a14d60f3bae2365fb89af19f4
Alejandro Barrios <ottoabarriosp@hotmail.com>
2023-06-29 17:40:16 -0400

    feat(server): define initial controller

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,5 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller()
 export class AppController {
@@ -8,5 +15,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  async upload(@UploadedFile() file: any) {
+    return 'ok';
   }
 }
